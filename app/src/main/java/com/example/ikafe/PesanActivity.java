bc8b3b80d6f1842a781edfa08a4306555a0d100a
Naufal Yudhistira <yudhistn@gmail.com>
2019-07-30 14:34:56 +0700

    initial commit

--- app/src/main/java/com/example/ikafe/PesanActivity.java
+++ app/src/main/java/com/example/ikafe/PesanActivity.java
@@ -2,30 +2,39 @@ package com.example.ikafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class PesanActivity extends AppCompatActivity {
 
     private Spinner dropKdMenu;
     private Button btnInput;
     private EditText edtTanggal, edtJam, edtNoMeja, edtHarga;
+    private Calendar myCalendar;
+    private
+    DatePickerDialog.OnDateSetListener date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesan);
-
+        myCalendar = Calendar.getInstance();
         String[] arrMenu = {" -> Pilih Menu","B01 - Kopi Hitam","B02 - Cappuccino","M03 - Sparkling Tea","F01 - Batagor","F02 - Cireng","F03 - Nasi Goreng","D01 - Cheese Cake","D02 - Black Salad"};
 
         dropKdMenu = findViewById(R.id.dropKdMenu);
@@ -36,10 +45,50 @@ public class PesanActivity extends AppCompatActivity {
         edtNoMeja = findViewById(R.id.edtNoMeja);
         edtHarga = findViewById(R.id.edtHarga);
 
+        date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+                edtTanggal.setText(sdf.format(myCalendar.getTime()));
+            }
+        };
+
         ArrayAdapter<String> menuAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_dropdown_item, arrMenu);
         menuAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropKdMenu.setAdapter(menuAdapter);
+
+        edtTanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(PesanActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        edtJam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar mCurtime = Calendar.getInstance();
+                int hour = mCurtime.get(Calendar.HOUR_OF_DAY);
+                int minute = mCurtime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(PesanActivity.this, new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        edtJam.setText(selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true);
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+            }
+        });
 
         btnInput.setOnClickListener(new View.OnClickListener() {
             @Override
